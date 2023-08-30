1304b0ca4cac980622dff2963790516bff535178
StanislavPB <perminovsb@gmail.com>
2023-08-30 12:21:30 +0300

    lesson3

---
+++ src/lesson3/Codelesson2.java
@@ -0,0 +1,17 @@
+package lesson3;
+
+public class Codelesson2 {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 6;
+        int result = a % b;
+
+        System.out.println(result);
+
+        double a1 = 19.6;
+        double b1 = 6.4;
+        double result1 = a1 % b1;
+
+        System.out.println(result1);
+    }
+}
---
+++ src/lesson3/Lessoncode.java
@@ -0,0 +1,48 @@
+package lesson3;
+
+public class Lessoncode {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+        int result = a / b;
+        System.out.println(result);
+
+        double x = 10;
+        double y = 4;
+        double result2 = x / y;
+        System.out.println(result2);
+
+        int a3 = 10;
+        int b3 = 4;
+        double result3 = a3 / b3;
+        System.out.println(result3);
+
+
+        double a3double = a3;
+        double b3double = b3;
+        double result3double = a3double / b3double;
+        System.out.println(result3double);
+
+
+
+        byte x1 = 100;
+        short x2 = x1;
+        int x3 = 100;
+
+        byte x4 = (byte) x3;
+
+        System.out.println(x4);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
