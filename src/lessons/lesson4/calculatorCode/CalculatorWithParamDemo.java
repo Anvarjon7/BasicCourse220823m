e697b38bf10de4ebc6fcad2ce509a38918e6f21b
StanislavPB <perminovsb@gmail.com>
2023-09-04 13:53:50 +0300

    lesson6

--- src/lessons/lesson4/calculatorCode/CalculatorWithParamDemo.java
+++ src/lessons/lesson4/calculatorCode/CalculatorWithParamDemo.java
@@ -4,12 +4,16 @@ public class CalculatorWithParamDemo {
     public static void main(String[] args) {
         CalculatorWithParameters calculator = new CalculatorWithParameters();
 
-        System.out.println("Результат сложения: "  + calculator.add(1000.5,400.1));
+        double rezult = calculator.add(1000.5,400.1);
+
+        System.out.println("Результат сложения: "  + rezult);
 
         double parameter1 = 500.5;
         double parameter2 = 100.5;
 
-        System.out.println("результат вычитания: " + calculator.sub(parameter1,parameter2));
+        rezult = calculator.add(parameter1,parameter2);
+
+        System.out.println("результат вычитания: " + rezult);
 
     }
 }
---
+++ src/lessons/lesson6/Cat.java
@@ -0,0 +1,10 @@
+package lessons.lesson6;
+
+public class Cat {
+
+    int age;
+
+    public Cat(int age) {
+        this.age = age;
+    }
+}
