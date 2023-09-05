aab590b81e6c1645569d81ed279e0cbac2ca7144
StanislavPB <perminovsb@gmail.com>
2023-09-05 13:45:30 +0300

    lesson7

---
+++ src/lessons/lesson7/calculator/userInterface/UserOutput.java
@@ -0,0 +1,8 @@
+package lessons.lesson7.calculator.userInterface;
+
+public class UserOutput {
+    public void printData(String message, Double dataForPrinting){
+        System.out.println(message);
+        System.out.println(" -> " + dataForPrinting);
+    }
+}
