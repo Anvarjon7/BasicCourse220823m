2c47b9e7d0b47c6c5d433d2f5441133eb64d9f1c
StanislavPB <perminovsb@gmail.com>
2023-09-05 12:48:23 +0300

    lesson7

--- src/lessons/lesson7/random/RandomExample.java
+++ src/lessons/lesson7/random/RandomExample.java
@@ -2,25 +2,30 @@ package lessons.lesson7.random;
 
 public class RandomExample {
     public static void main(String[] args) {
-        /*
-            Math.random() *  (max - min + 1) + min
-         */
-        double random1 =  Math.random() *  (50 - 10 + 1) + 10;
 
-        double random2 =  Math.random() *  (100 - 0 + 1) + 0;
-        double random3 =  Math.random() *  (150 - 120 + 1) + 120;
-        double random4 =  Math.random() *  (1000 - 550 + 1) + 550;
-        double random5 =  Math.random() *  (1000000 - 150000 + 1) + 150000;
-
-        long intRandom1 = Math.round(random1);
-
-
-        System.out.println("1st random example : "+ intRandom1 );
-        System.out.println("2st random example : "+ Math.round(random2)  );
-        System.out.println("3st random example : "+ Math.round(random3) );
-        System.out.println("4st random example : "+ Math.round(random4) );
-        System.out.println("5st random example : "+ Math.round(random5) );
+        double random1 = Math.random();
 
         System.out.println(random1);
+
+        /*
+        получить случайное целое число в диапазоне от min до max
+
+        min + Math.random() * (max - min +1)
+
+        min = 0, max = 100
+
+        0 + random * (100 - 0 + 1) ->
+        если random = 0 -> значение будет 0
+        если random = 0.9999999 -> значение будет 100
+
+         */
+
+        System.out.println((int) (random1 * 100));
+
+        double random2 = Math.random() * (50 -10 +1) + 10;
+
+        System.out.println((int) random2);
+
     }
+
 }
