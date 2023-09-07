2c8adc88ca814b5a33bfba54e9cf613c10f56515
StanislavPB <perminovsb@gmail.com>
2023-09-07 11:24:26 +0300

    lesson 9

--- src/lessons/lesson9/prctice/task1/Dog.java
+++ src/lessons/lesson9/prctice/task1/Dog.java
@@ -2,4 +2,16 @@ package lessons.lesson9.prctice.task1;
 
 public class Dog {
 
+    String dogName;
+
+    public Dog(String dogName) {
+        this.dogName = dogName;
+    }
+
+    public void voice() {
+        System.out.println(dogName);
+        System.out.println(dogName);
+        System.out.println(dogName);
+
+    }
 }
---
+++ src/lessons/lesson9/prctice/task1/DogDemo.java
@@ -0,0 +1,9 @@
+package lessons.lesson9.prctice.task1;
+
+public class DogDemo {
+    public static void main(String[] args) {
+
+        Dog myDog = new Dog("Zeus");
+        myDog.voice();
+    }
+}
