943a2103579803a35056921ed0a17e398a96e895
StanislavPB <perminovsb@gmail.com>
2023-09-07 13:19:46 +0300

    lesson 9

--- src/lessons/lesson9/prctice/task1/Dog.java
+++ src/lessons/lesson9/prctice/task1/Dog.java
@@ -3,20 +3,40 @@ package lessons.lesson9.prctice.task1;
 public class Dog {
 
     String dogName;
-
     int age;
+    String color;
+    double weight;
 
-    public Dog(String dogName, int age) {
+    public Dog(String dogName, int age, String color, double weight) {
         this.dogName = dogName;
         this.age = age;
+        this.color = color;
+        this.weight = weight;
     }
 
     public void voice() {
-        System.out.println(dogName + " " + age);
+       // System.out.println(dogName + " " + age + " " + color);
+        System.out.println("кличка собаки: " + dogName);
+        System.out.println("возраст собаки: " + age);
+        System.out.println("цвет собаки: " + color);
+        System.out.println("вес собаки: " + weight);
+
+        System.out.println();
     }
 
     public void happyBirthday(){
         age++; // равносильно age = age + 1;
-
     }
+
+    void changeColor(String color) {
+        // напишите тут команду сохранения нового цвета в свойство класса
+        this.color = color;
+    }
+
+    public void changeWeight(double changeValue){
+        weight = weight + changeValue;
+    }
+
+
+
 }
--- src/lessons/lesson9/prctice/task1/DogDemo.java
+++ src/lessons/lesson9/prctice/task1/DogDemo.java
@@ -3,9 +3,14 @@ package lessons.lesson9.prctice.task1;
 public class DogDemo {
     public static void main(String[] args) {
 
-        Dog myDog = new Dog("Zeus", 2);
+        Dog myDog = new Dog("Zeus", 2, "grey-tiger", 50.5);
         myDog.voice();
         myDog.happyBirthday();
         myDog.voice();
+        myDog.changeColor("tiger");
+        myDog.voice();
+        myDog.changeWeight(-1.5);
+        myDog.voice();
+
     }
 }
