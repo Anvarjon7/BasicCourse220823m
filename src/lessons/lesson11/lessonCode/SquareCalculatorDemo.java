b5e25a657ec3c348270bc391164fc8e46ff1d828
StanislavPB <perminovsb@gmail.com>
2023-09-11 13:03:25 +0300

    lesson11

--- src/lessons/lesson11/lessonCode/SquareCalculatorDemo.java
+++ src/lessons/lesson11/lessonCode/SquareCalculatorDemo.java
@@ -18,7 +18,24 @@ public class SquareCalculatorDemo {
             case 1:
                 userData.correctAnswer = areaCalculator.circleArea(userData.shapeParam1);
                 break;
+            case 2:
+                userData.correctAnswer = areaCalculator.squareArea(userData.shapeParam1);
+                break;
+            case 3:
+                userData.correctAnswer = areaCalculator.rectangleArea(userData.shapeParam1, userData.shapeParam2);
+                break;
         }
+
+        // блок анализа ответов и сравнения
+
+        userData.checkResult = (userData.userAnswer == userData.correctAnswer);
+
+        if (userData.checkResult) {
+            System.out.println("Ваш ответ правильный! МОЛОДЕЦ!!!");
+        } else {
+            System.out.println("Ваш ответ НЕ ПРАВИЛЬНЫЙ (((");
+        }
+
 
 
 
