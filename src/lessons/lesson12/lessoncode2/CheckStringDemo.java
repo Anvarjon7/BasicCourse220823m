515608f6fdf1fe4fe6f1678704107e9bf94f7c03
StanislavPB <perminovsb@gmail.com>
2023-09-12 13:46:28 +0300

    lesson12

--- src/lessons/lesson12/lessoncode2/CheckStringDemo.java
+++ src/lessons/lesson12/lessoncode2/CheckStringDemo.java
@@ -2,18 +2,17 @@ package lessons.lesson12.lessoncode2;
 
 public class CheckStringDemo {
     public static void main(String[] args) {
+        TextInput textInput = new TextInput();
+        CheckString checkString = new CheckString();
+        TextOutput textOutput = new TextOutput();
 
         // блок ввода строки от пользователя
-        TextInput textInput = new TextInput();
         String userText = textInput.inputTextLine();
 
         // блок вычисления длины строки и его проверки
-
-        CheckString checkString = new CheckString();
         boolean checkResult = checkString.checkText(userText);
 
         // блок вывода информации пользователю
-        TextOutput textOutput = new TextOutput();
         textOutput.printInfo(userText, checkResult);
 
 
