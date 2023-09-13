14a1ca37f1156acf0ecfbd4bfd2e88b26884bc65
StanislavPB <perminovsb@gmail.com>
2023-09-13 12:01:34 +0300

    lesson 13

---
+++ src/lessons/lesson13/loop/whileLoop/LoopWhileExample2.java
@@ -0,0 +1,21 @@
+package lessons.lesson13.loop.whileLoop;
+
+public class LoopWhileExample2 {
+    public static void main(String[] args) {
+
+        double x = 10;
+        double y = 100;
+
+        while (x <= 100) {
+            System.out.println(" x = " + x);
+            System.out.println(" y = " + y);
+            System.out.println("Div result " + (y / x));
+            System.out.println();
+
+            x = x + 10;
+        }
+
+        System.out.println("Stop loop. X = " + x);
+
+    }
+}
