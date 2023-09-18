6b5e4d05929cc9ebe95832b3365c5845a8b43dd6
StanislavPB <perminovsb@gmail.com>
2023-09-18 12:05:24 +0300

    lesson 16

---
+++ src/lessons/lesson16/practice/PointDemo2.java
@@ -0,0 +1,12 @@
+package lessons.lesson16.practice;
+
+
+import lessons.lesson16.lessonCode.Point;
+
+public class PointDemo2 {
+    public static void main(String[] args) {
+        Point newPoint = new Point();
+        newPoint.x = 15;
+
+    }
+}
