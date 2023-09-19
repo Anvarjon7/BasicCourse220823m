4b3b947c607533efaba4dde2b12e70b67057026b
StanislavPB <perminovsb@gmail.com>
2023-09-19 10:56:48 +0300

    lesson 16

--- src/lessons/lesson16/raceExample/Race.java
+++ src/lessons/lesson16/raceExample/Race.java
@@ -11,4 +11,24 @@ public class Race {
       double raceResult = distance / ourCar.getMaxSpeed();
       return raceResult;
     }
+
+    public Car calculateRaceResult(Car car1, Car car2, Car car3){
+        double resultCar1 = raceResultCalculate(car1);
+        double resultCar2 = raceResultCalculate(car2);
+        double resultCar3 = raceResultCalculate(car3);
+
+        if (resultCar1 < resultCar2 && resultCar1 < resultCar3) {
+            return car1;
+        } else if (resultCar2 < resultCar1 && resultCar2 < resultCar3) {
+            return car2;
+        }  else {
+            return car3;
+        }
+    }
+
+
+
+    public void showWinner(Car carWinner){
+        System.out.println("Победил автомобиль " + carWinner.getProducer());
+    }
 }
--- src/lessons/lesson16/raceExample/RaceApp.java
+++ src/lessons/lesson16/raceExample/RaceApp.java
@@ -8,33 +8,37 @@ public class RaceApp {
 
         Car car2 = new Car("BMW", "M5", 330);
 
-        Car car3 = new Car("Lamborghini", "Diablo", 380);
+        Car car3 = new Car("Lamborghini", "Diablo", 320);
 
         Race raceMonaco = new Race("Monaco", 75);
 
         // блок расчета результата
 
-        double resultCar1 = raceMonaco.raceResultCalculate(car1);
-        double resultCar2 = raceMonaco.raceResultCalculate(car2);
-        double resultCar3 = raceMonaco.raceResultCalculate(car3);
+        Car winnerCar = raceMonaco.calculateRaceResult(car1, car2, car3);
+
+//
+//        double resultCar1 = raceMonaco.raceResultCalculate(car1);
+//        double resultCar2 = raceMonaco.raceResultCalculate(car2);
+//        double resultCar3 = raceMonaco.raceResultCalculate(car3);
 
         // вызов некоего стороннего метода который будет возвращать
         // результат гонки
 
 
         // блок вывода результата пользователю
+         raceMonaco.showWinner(winnerCar);
 
-        if (resultCar1 < resultCar2 && resultCar1 < resultCar3) {
-            System.out.println("Победил автомобиль " + car1.getProducer());
-        }
-
-        if (resultCar2 < resultCar1 && resultCar2 < resultCar3) {
-            System.out.println("Победил автомобиль " + car2.getProducer());
-        }
-
-        if (resultCar3 < resultCar1 && resultCar3 < resultCar2) {
-            System.out.println("Победил автомобиль " + car3.getProducer());
-        }
+//        if (resultCar1 < resultCar2 && resultCar1 < resultCar3) {
+//            System.out.println("Победил автомобиль " + car1.getProducer());
+//        }
+//
+//        if (resultCar2 < resultCar1 && resultCar2 < resultCar3) {
+//            System.out.println("Победил автомобиль " + car2.getProducer());
+//        }
+//
+//        if (resultCar3 < resultCar1 && resultCar3 < resultCar2) {
+//            System.out.println("Победил автомобиль " + car3.getProducer());
+//        }
 
     }
 }
@@ -42,6 +46,6 @@ public class RaceApp {
 /*
 скопировать себе код и модифицировать его таким образом чтобы:
 вычисление результата гонки осуществлялось отдельным методом
-и вывод результата состоял не из трех блок if {} а просто System.out.println
+и вывод результата состоял не из трех блоков if {} а просто System.out.println
 
  */
